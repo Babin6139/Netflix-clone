7552ebed0ccf3016239eb635c8a793451a8d07a9
Babin6139 <khatribabin39@gmail.com>
2023-02-25 18:59:03 +0545

    fix: image links are changed and the code is changed accordingly

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom';
 import "./Nav.css"
+import netflix from "./images/netflix.png"
 function Nav() {
     const [show, handleshow] = useState(false);
     const transitionNavBar=()=>{
@@ -22,7 +23,7 @@ function Nav() {
             <img 
                 onClick={()=>history.push("/")}
                 className="nav__logo"
-                src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+                src={netflix}
                 alt=""
             />
             <img
